83cbcd551d0338401ca6e4126fb60fe2fac13a65
Ali Reda <alyredagomaa@gmail.com>
2024-06-03 11:11:29 +0300

    using jquery in our app

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,9 @@
+// using axios in our app  
 import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// using jquery in our app
+import $ from 'jquery';
+window.$ = window.jQuery = $;
